ff731f07d08e51f580008d2d6e0a81d14d635eb3
Noided <102483465+n01ded@users.noreply.github.com>
2022-11-14 21:50:38 +0100

    Add files via upload

---
+++ gentooVM/post_chroot.sh
@@ -0,0 +1,287 @@
+#!/bin/bash
+LIGHTRED='\033[1;91m'
+LIGHTGREEN='\033[1;32m'
+source /etc/profile
+cd gentooVM
+scriptdir=$(pwd)
+cd ..
+sed -i '/^$/d' install_vars
+#rm -rf /mnt/gentoo/install_vars
+#cat /temp_f >> /mnt/gentoo/install_vars
+#rm -rf /mnt/gentoo/temp_f
+install_vars=install_vars
+
+install_vars_count="$(wc -w /install_vars)"
+disk=$(sed '1q;d' install_vars)
+username=$(sed '2q;d' install_vars)
+kernelanswer=$(sed '3q;d' install_vars)
+hostname=$(sed '4q;d' install_vars)
+cpus=$(sed '5q;d' install_vars)
+part_swap=$(sed '6q;d' install_vars)
+part_1=$(sed '7q;d' install_vars)
+part_2=$(sed '8q;d' install_vars)
+part_root=$(sed '9q;d' install_vars)
+part_home=$(sed '10q;d' install_vars)
+performance_opts=$(sed '11q;d' install_vars)
+#This line is getting written to line 13 for some reason
+nw_interface=$(sed '12q;d' install_vars)
+dev_sd=("/dev/$disk")
+mount $part_2 /boot
+jobs=("-j${cpus}")
+#printf "jobs equals %s" % $jobs
+printf "mounted boot\n"
+mount $part_home /home
+#TODO everything below this point fails on musl, figure out why, error is Your current profile is invalid
+emerge --sync --quiet
+#TODO This emerge fails for some reason
+emerge -q app-portage/mirrorselect
+emerge -q gentoolkit
+printf "searching for fastest servers\n"
+mirrorselect -s5 -b10 -D
+printf "sync complete\n"
+sleep 10
+filename=gentootype.txt
+line=$(head -n 1 $filename)
+#Checks what type of stage was used, and installs necessary overlays
+case $line in
+  latest-stage3-amd64-hardened)
+    #TODO put stuff specific to gcc hardened here
+    emerge --verbose --update --deep --newuse --quiet @world
+    printf "big emerge complete\n"
+    ;;
+  latest-stage3-amd64-musl-hardened)
+    eselect profile set --force 31
+    echo "dev-vcs/git -gpg" >> /etc/portage/package.use
+    emerge -uvNDq @world
+    ;;
+  latest-stage3-amd64-musl-vanilla)
+    eselect profile set --force 30
+    echo "dev-vcs/git -gpg" >> /etc/portage/package.use
+    emerge -uvNDq @world
+    ;;
+  latest-stage3-amd64)
+    emerge -uvNDq @world
+    printf "big emerge complete\n"
+    ;;
+esac
+
+
+printf "preparing to do big emerge\n"
+
+printf "Europe/Paris\n" > /etc/timezone
+emerge --config --quiet sys-libs/timezone-data
+printf "timezone data emerged\n"
+#en_US.UTF-8 UTF-8
+printf "fr_FR.UTF-8 UTF-8\n" >> /etc/locale.gen
+locale-gen
+printf "script complete\n"
+eselect locale set 4
+env-update && source /etc/profile
+
+#Installs the kernel
+printf "preparing to emerge kernel sources\n"
+emerge -q sys-kernel/gentoo-sources
+wait
+sleep 10
+ls -l /usr/src/linux
+cd /usr/src/linux
+emerge -q sys-apps/pciutils
+emerge -q app-arch/lzop
+emerge -q app-arch/lz4
+emerge --autounmask-continue -q sys-kernel/genkernel
+emerge app-eselect/eselect-repository
+if [ $kernelanswer = "no" ]; then
+	#cp /gentooVM/gentoo/kernel/gentoominimal /root/kernel.config
+	#genkernel --kernel-config=/root/kernel.config all
+	cp /gentooVM/gentoo/kernel/gentoominimal /usr/src/linux/.config
+	make $jobs && make modules_install
+	make install
+	#mv /root/kernel.config /usr/src/linux/.config
+	printf "Kernel installed\n"
+elif [ $kernelanswer = "edit" ]; then
+    make mrproper
+	cp /gentooVM/gentoo/kernel/gentoominimal /usr/src/linux/.config
+	make menuconfig
+	make $jobs && make modules_install
+	make install
+	printf "Kernel installed\n"
+else
+	printf "time to configure your own kernel\n"
+	make menuconfig
+	make $jobs && make modules_installl
+	make install
+	printf "Kernel installed\n"
+fi
+genkernel --install --kernel-config=/usr/src/linux/.config initramfs
+
+cd /etc/init.d
+#enables DHCP
+sed -i -e "s/localhost/$hostname/g" /etc/conf.d/hostname
+emerge --noreplace --quiet net-misc/netifrc
+nw_config_str=("config_${nw_interface}=\"dhcp\"")
+printf "$nw_config_str\n" >> /etc/conf.d/net
+if [ $install_vars_count -gt 12 ]; then
+    nw_interface2=$(sed '13q;d' install_vars)
+    nw_config_str3=("config_${nw_interface2}=\"dhcp\"")
+    printf "$nw_config_str3\n" >> /etc/conf.d/net
+    net_config_str4=("net.${nw_interface2}")
+    ln -s net.lo $net_config_str4
+    rc-update add $net_config_str4 default
+    rc-update add elogind boot
+else
+    printf "only 1 network device found\n"
+fi
+lscpu >> install_vars
+UUID2=$(blkid -s UUID -o value $part_2)
+UUID2=("UUID=${UUID2}")
+UUID3=$(blkid -s UUID -o value $part_swap)
+UUID3=("UUID=${UUID3}")
+UUID4=$(blkid -s UUID -o value $part_root)
+UUID4=("UUID=${UUID4}")
+UUID4=$(blkid -s UUID -o value $part_home)
+UUID4=("UUID=${UUID5}")
+printf "%s\t\t/boot/efi\tvfat\t\tdefaults\t0 2\n" $UUID2 >> /etc/fstab
+SUB_STR='/dev/'
+if [[ "$part_3" == *"$SUB_STR"* ]]; then
+    printf "%s\t\tnone\t\tswap\t\tsw\t\t0 0\n" $UUID3 >> /etc/fstab
+fi
+printf "%s\t\t/\t\text4\t\tnoatime\t\t0 1\n" $UUID4 >> /etc/fstab
+printf "%s\t\t/home\t\text4\t\tnoatime\t\t0 1\n" $UUID4 >> /etc/fstab
+net_config_str2=("net.${nw_interface}")
+ln -s net.lo $net_config_str2
+rc-update add $net_config_str2 default
+printf "dhcp enabled\n"
+emerge -q app-admin/doas
+rm -rf /etc/doas.conf
+cd $scriptdir
+cp doas.conf /etc/
+printf "installed doas and enabled it for wheel group\n"
+emerge -q sys-apps/mlocate
+emerge -q net-misc/dhcpcd
+
+#installs grub
+emerge --verbose -q sys-boot/grub:2
+grub-install --target=x86_64-efi --efi-directory=/boot
+grub-mkconfig -o /boot/grub/grub.cfg
+printf "updated grub\n"
+printf "run commands manually from here on to see what breaks\n"
+##use this for MBR# grub-install $dev_sd
+useradd -m -G users,wheel,audio -s /bin/bash $username
+printf "added user\n"
+cd ..
+mv gentooVM.zip /home/$username
+##rm -rf /gentooVM
+stage3=$(ls stage3*)
+rm -rf $stage3
+
+if [ $performance_opts = "yes" ]; then
+#ADDED TODAY START
+    emerge --autounmask-continue -UD @world
+    emerge dev-vcs/git
+    eselect repository enable mv
+    eselect repository enable lto-overlay
+    emerge --sync
+    emerge --oneshot --quiet sys-devel/gcc
+    gcc-config 2
+    emerge --oneshot --usepkg=n --quiet sys-devel/libtool
+    emerge --autounmask-continue app-text/texlive
+    emerge -q dev-libs/isl
+    cd /bin
+    git clone https://github.com/periscop/cloog
+    cd cloog
+    GET_SUBMODULES="/bin/cloog/get_submodules.sh"
+    . "$GET_SUBMODULES"
+    AUTOGEN="/bin/cloog/autogen.sh"
+    . "$AUTOGEN"
+    CONFIG="/bin/cloog/configure"
+    bash "$CONFIG"
+    make && make install
+    echo "dev-lang/python::lto-overlay ~amd64" >> /etc/portage/package.accept_keywords
+    echo "dev-lang/python::lto-overlay ~amd64" >> /etc/portage/package.accept_keywords
+    echo "virtual/freedesktop-icon-theme::mv ~amd64" >> /etc/portage/package.accept_keywords
+    echo "app-portage/eix::mv ~amd64" >> /etc/portage/package.accept_keywords
+    echo "app-shells/push::mv ~amd64" >> /etc/portage/package.accept_keywords
+    echo "app-shells/quoter::mv ~amd64" >> /etc/portage/package.accept_keywords
+    echo "app-text/lesspipe::mv ~amd64" >> /etc/portage/package.accept_keywords
+    echo "sys-apps/less::mv ~amd64" >> /etc/portage/package.accept_keywords
+    echo "x11-libs/gtk+::mv ~amd64" >> /etc/portage/package.accept_keywords
+    echo "virtual/man::mv ~amd64" >> /etc/portage/package.accept_keywords
+    echo "sys-apps/less:mv ~amd64" >> /etc/portage/package.accept_keywords
+    echo "app-text/lesspipe:mv ~amd64" >> /etc/portage/package.accept_keywords
+    echo "app-shells/quoter:mv ~amd64" >> /etc/portage/package.accept_keywords
+    echo "app-shells/push:mv ~amd64" >> /etc/portage/package.accept_keywords
+    echo "app-portage/eix:mv ~amd64" >> /etc/portage/package.accept_keywords
+    emerge -q sys-config/ltoize
+    emerge -q app-portage/portage-bashrc-mv
+    emerge -q app-portage/eix
+    emerge -q app-portage/lto-rebuild
+    emerge -q app-shells/runtitle
+    emerge -q app-shells/push
+    emerge -q app-shells/quoter
+    emerge -q app-text/lesspipe
+    emerge -q sys-apps/less
+    emerge -q virtual/freedesktop-icon-theme
+    emerge -q virtual/man
+    emerge -q dev-lang/python
+    #TODO add option to append -falign-functions=32 to CFLAGS if user has an Intel Processor
+    if grep -Fq "$GenuineIntel" install_vars
+    then
+        sed -i 's/^CFLAGS=\"${COMMON_FLAGS}\"/CFLAGS=\"-march=native ${CFLAGS} -pipe -falign-functions=32\"/g' /etc/portage/make.conf
+    else
+        sed -i 's/^CFLAGS=\"${COMMON_FLAGS}\"/CFLAGS=\"-march=native ${CFLAGS} -pipe\"/g' /etc/portage/make.conf
+    fi
+    sed -i 's/CXXFLAGS=\"${COMMON_FLAGS}\"/CXXFLAGS=\"${CFLAGS}\"/g' /etc/portage/make.conf
+    sed -i "5s/^/NTHREADS=\"$cpus\"\n/" /etc/portage/make.conf
+    sed -i '6s/^/source make.conf.lto\n\n/' /etc/portage/make.conf
+    sed -i '11s/^/LDFLAGS=\"${CFLAGS} -fuse-linker-plugin\"\n/' /etc/portage/make.conf
+    sed -i '12s/^/CPU_FLAGS_X86=\"aes avx avx2 f16c fma3 mmx mmxext pclmul popcnt sse sse2 sse3 sse4_1 sse4_2 ssse3\"\n/' /etc/portage/make.conf
+    sed -i 's/-quicktime/-quicktime lto/g' /etc/portage/make.conf
+    sed -i 's/-clamav/-clamav graphite/g' /etc/portage/make.conf
+    emerge gcc
+    emerge dev-util/pkgconf
+    emerge -eq --keep-going @world
+    printf "performance enhancements setup, you'll have to emerge sys-config/ltoize to complete\n"
+elif [ $performance_opts = "no" ]; then
+    printf "performance optimization not selected\n"
+fi
+
+while true; do
+    printf ${LIGHTGREEN}"enter the password for your root user\n>"
+    read -s password
+    printf ${LIGHTGREEN}"re-enter the password for your root user\n>"
+    read -s password_compare
+    if [ "$password" = "$password_compare" ]; then
+	echo "root:$password" | chpasswd
+        break
+    else
+        printf ${LIGHTRED}"passwords do not match, re enter them\n"
+        printf ${WHITE}".\n"
+        sleep 3
+        clear
+    fi
+done
+while true; do
+    printf ${LIGHTGREEN}"enter the password for your user %s\n>" $username
+    read -s password
+    printf ${LIGHTGREEN}"re-enter the password for %s\n>" "$username"
+    read -s password_compare
+    if [ "$password" = "$password_compare" ]; then
+	echo "$username:$password" | chpasswd
+        break
+    else
+        printf ${LIGHTRED}"passwords do not match, re enter them\n"
+        printf ${WHITE}".\n"
+        sleep 3
+        clear
+    fi
+done
+printf "cleaning up\n"
+rm -rf /gentootype.txt
+rm -rf /install_vars
+cp -r /gentooVM/gentoo/portage/savedconfig /etc/portage/
+cp -r /gentooVM/gentoo/portage/env /etc/portage/
+cp /gentooVM/gentoo/portage/package.env /etc/portage/
+rm -rf /gentooVM
+printf ${LIGHTGREEN}"You now have a completed gentoo installation system, reboot and remove the installation media to load it\n"
+printf ${LIGHTGREEN}"reboot\n"
+rm -rf /post_chroot.sh
---
+++ gentooVM/setup_gentoo.sh
@@ -0,0 +1,244 @@
+#!/bin/bash
+#this puts some things in place like your make.conf, aswell as package.use
+LIGHTGREEN='\033[1;32m'
+LIGHTRED='\033[1;91m'
+WHITE='\033[1;97m'
+MAGENTA='\033[1;35m'
+CYAN='\033[1;96m'
+cd ..
+start_dir=$(pwd)
+printf ${MAGENTA}"MAKE SURE YOUR ROOT PARTITION IS THE 2ND ONE ON THE DEVICE YOU'LL BE INSTALLING TO\n\n"
+fdisk -l >> devices
+ifconfig -s >> nw_devices
+cut -d ' ' -f1 nw_devices >> network_devices
+rm -rf nw_devices
+sed -e "s/lo//g" -i network_devices
+sed -e "s/Iface//g" -i network_devices
+sed '/^$/d' network_devices
+sed -e '\#Disk /dev/ram#,+5d' -i devices
+sed -e '\#Disk /dev/loop#,+5d' -i devices
+
+cat devices
+while true; do
+    printf ${CYAN}"Enter the device name you want to install gentoo on (ex, sda for /dev/sda)\n>"
+    read disk
+    disk="${disk,,}"
+    partition_count="$(grep -o $disk devices | wc -l)"
+    disk_chk=("/dev/${disk}")
+    if grep "$disk_chk" devices; then
+        printf "Would you like to auto provision %s? \n This will create a GPT partition scheme where\n%s1 = 2 MB bios_partition\n%s2 = 128 MB boot partition\n%s3 x GB lvm partition (the rest of the hard disk) WITH FULL DISK ENCRYPTION\n\nEnter y to continue with auto provision or n to exit the script \n>" $disk_chk $disk_chk $disk_chk $disk_chk $disk_chk
+        read auto_prov_ans
+        if [ "$auto_prov_ans" = "y" ]; then
+            wipefs -a $disk_chk
+            parted -a optimal $disk_chk --script mklabel gpt
+            parted $disk_chk --script mkpart primary 1MiB 3MiB
+            parted $disk_chk --script name 1 grub
+            parted $disk_chk --script set 1 bios_grub on
+            parted $disk_chk --script mkpart primary 3MiB 131MiB
+            parted $disk_chk --script name 2 boot
+            parted $disk_chk --script set 2 boot on
+            parted -a optimal $disk_chk --script mkpart primary 131MiB -1
+            parted $disk_chk --script set 2 lvm on
+            part_1=("${disk_chk}p1")
+            part_2=("${disk_chk}p2")
+            part_3=("${disk_chk}p3")
+            mkfs.fat -F 32 $part_2
+            #mkfs.ext4 $part_2
+            cryptsetup benchmark
+            cryptsetup --verbose --type luks1 --cipher serpent-xts-plain64 --key-size 512 --hash whirlpool --iter-time 10000 --use-random --verify-passphrase luksFormat $part_3
+            cryptsetup luksOpen $part_3 root
+            pvcreate /dev/mapper/root
+            vgcreate gentooVG /dev/mapper/root
+            lvcreate -L 8GiB gentooVG -n gentooSwap
+            lvcreate -L 24GiB gentooVG -n gentooRoot
+            lvcreate -l +100%FREE gentooVG -n gentooHome
+            vgchange --available y
+            mkswap /dev/mapper/gentooVG-gentooSwap
+            mkfs.ext4 /dev/mapper/gentooVG-gentooRoot
+            mkfs.ext4 /dev/mapper/gentooVG-gentooHome
+            swapon /dev/mapper/gentooVG-gentooSwap
+            swap_part=("/dev/mapper/gentooVG-gentooSwap")
+            root_part=("/dev/mapper/gentooVG-gentooRoot")
+            home_part=("/dev/mapper/gentooVG-gentooHome")
+            rm -rf devices
+            clear
+            sleep 2
+            break
+        elif [ "$auto_prov_ans" = "n" ]; then
+            printf ${CYAN}"Enter the partition number for root (ex, 2 for /dev/sda2)\n>"
+            read num
+            rootpart="$disk$num"
+            if grep "$rootpart" devices; then
+                #continue running the script
+                if [ $partition_count -gt 2 ]; then
+                    printf "do you want to enable swap?\n>"
+                    read swap_answer
+                fi
+                swap_answer="${swap_answer,,}"
+                if [ "$swap_answer" = "no" ]; then
+                    printf "not using swap"
+                    part_3="no"
+                else
+                    while true; do
+                        printf "enter swap partition (ex, /dev/sda3)\n>"
+                        read part_3
+                        part_3="${part_3,,}"
+                        if grep "$part_3" devices; then
+                            mkswap $part_3
+                            swapon $part_3
+                            break
+                        else
+                            printf${LIGHTRED}"%s is not a valid swap partition, review this list of your devices and make a valid selection\n" $part_3
+                            printf ${WHITE}".\n"
+                            sleep 5
+                            clear
+                            cat devices
+                        fi
+                    done
+                fi
+                printf ${LIGHTGREEN}"%s is valid :D continuing with the script\n" $rootpart
+                break
+            else
+                #rootpartnotfound
+                printf ${LIGHTRED}"%s is not a valid installation target, review this list of your devices and make a valid selection\n" $rootpart
+                printf ${WHITE}".\n"
+                sleep 5
+                clear
+            fi
+        else
+            printf ${LIGHTRED}"%s is an invalid answer, do it correctly" $auto_prov_ans
+            printf ${WHITE}".\n"
+            sleep 2
+        fi
+    else
+        printf ${LIGHTRED}"%s is an invalid device, try again with a correct one\n" $disk_chk
+        printf ${WHITE}".\n"
+        sleep 5
+        clear
+        cat devices
+    fi
+done
+
+printf "enter a number for the stage 3 you want to use\n"
+printf "0 = regular\n1 = regular hardened\n2 = hardened musl\n3 = vanilla musl\n>"
+read stage3select
+printf ${CYAN}"Enter the username for your NON ROOT user\n>"
+#There is a possibility this won't work since the handbook creates a user after rebooting and logging as root
+read username
+username="${username,,}"
+printf ${CYAN}"Enter Yes to make a kernel from scratch, edit to edit the hardened config, or No to use the default hardened config\n>"
+read kernelanswer
+kernelanswer="${kernelanswer,,}"
+printf ${CYAN}"Enter the Hostname you want to use\n>"
+read hostname
+printf ${CYAN}"Do you want to do performance optimizations. LTO -O3 and Graphite?(yes or no)\n>"
+read performance_opts
+performance_opts="${performance_opts,,}"
+printf ${LIGHTGREEN}"Beginning installation, this will take several minutes\n"
+
+#copying files into place
+mount $root_part /mnt/gentoo
+mv gentooVM /mnt/gentoo
+mv gentooVM.zip /mnt/gentoo/
+mv network_devices /mnt/gentoo/gentooVM/
+cd /mnt/gentoo/gentooVM
+
+install_vars=/mnt/gentoo/gentooVM/install_vars
+cpus=$(grep -c ^processor /proc/cpuinfo)
+pluscpus=$((cpus+1))
+echo "$disk" >> "$install_vars"
+echo "$username" >> "$install_vars"
+echo "$kernelanswer" >> "$install_vars"
+echo "$hostname" >> "$install_vars"
+echo "$cpus" >> "$install_vars"
+echo "$part_swap" >> "$install_vars"
+echo "$part_1" >> "$install_vars"
+echo "$part_2" >> "$install_vars"
+echo "$part_root" >> "$install_vars"
+echo "$part_home" >> "$install_vars"
+echo "$performance_opts" >> "$install_vars"
+cat network_devices >> "$install_vars"
+rm -f network_devices
+
+case $stage3select in
+  0)
+    GENTOO_TYPE=latest-stage3-amd64
+    ;;
+  1)
+    GENTOO_TYPE=latest-stage3-amd64-hardened
+    ;;
+  2)
+    GENTOO_TYPE=latest-stage3-amd64-musl-hardened
+    ;;
+  3)
+    GENTOO_TYPE=latest-stage3-amd64-musl-vanilla
+    ;;
+esac
+
+STAGE3_PATH_URL=http://distfiles.gentoo.org/releases/amd64/autobuilds/$GENTOO_TYPE.txt
+STAGE3_PATH=$(curl -s $STAGE3_PATH_URL | grep -v "^#" | cut -d" " -f1)
+STAGE3_URL=http://distfiles.gentoo.org/releases/amd64/autobuilds/$STAGE3_PATH
+touch /mnt/gentoo/gentootype.txt
+echo $GENTOO_TYPE >> /mnt/gentoo/gentootype.txt
+cd /mnt/gentoo/
+while [ 1 ]; do
+	wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 $STAGE3_URL
+	if [ $? = 0 ]; then break; fi;
+	sleep 1s;
+done;
+check_file_exists () {
+	file=$1
+	if [ -e $file ]; then
+		exists=true
+	else
+		printf "%s doesn't exist\n" $file
+		wget --tries=20 $STAGE3_URL
+		exists=false
+		$2
+	fi
+}
+
+check_file_exists /mnt/gentoo/stage3*
+stage3=$(ls /mnt/gentoo/stage3*)
+tar xpvf $stage3 --xattrs-include='*.*' --numeric-owner
+printf "unpacked stage 3\n"
+
+#rm -rf /mnt/gentoo/etc/portage
+cd /mnt/gentoo/gentooVM/gentoo/
+cp -a /mnt/gentoo/gentooVM/gentoo/portage/package.use/. /mnt/gentoo/etc/portage/package.use/
+cd /mnt/gentoo/
+rm -rf /mnt/gentoo/etc/portage/make.conf
+##copies our pre-made make.conf over
+cp /mnt/gentoo/gentooVM/gentoo/portage/make.conf /mnt/gentoo/etc/portage/
+printf "copied new make.conf to /etc/portage/\n"
+printf "there are %s cpus\n" $cpus
+sed -i "s/MAKEOPTS=\"-j3\"/MAKEOPTS=\"-j$pluscpus -l$cpus\"/g" /mnt/gentoo/etc/portage/make.conf
+sed -i "s/--jobs=3  --load-average=3/--jobs=$cpus  --load-average=$cpus/g" /mnt/gentoo/etc/portage/make.conf
+printf "moved portage files into place\n"
+
+#cp /mnt/gentoo/gentooVM/gentoo/portage/linux_drivers /mnt/gentoo/etc/portage/
+cp /mnt/gentoo/gentooVM/gentoo/portage/package.license /mnt/gentoo/etc/portage
+#cp /mnt/gentoo/gentooVM/gentoo/portage/package.accept_keywords /mnt/gentoo/etc/portage/
+#cp -r /mnt/gentoo/gentooVM/gentoo/portage/profile /mnt/gentoo/etc/portage/
+
+mkdir --parents /mnt/gentoo/etc/portage/repos.conf
+cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
+printf "copied gentoo repository to repos.conf\n"
+#
+##copy DNS info
+cp --dereference /etc/resolv.conf /mnt/gentoo/etc
+printf "copied over DNS info\n"
+
+cp /mnt/gentoo/gentooVM/post_chroot.sh /mnt/gentoo/
+cp /mnt/gentoo/gentooVM/install_vars /mnt/gentoo/
+printf "copied post_chroot.sh to /mnt/gentoo\n"
+chmod +x /mnt/gentoo/post_chroot.sh
+
+mount --types proc /proc /mnt/gentoo/proc
+mount --rbind /sys /mnt/gentoo/sys
+mount --make-rslave /mnt/gentoo/sys
+mount --rbind /dev /mnt/gentoo/dev
+mount --make-rslave /mnt/gentoo/dev
+
+cd /mnt/gentoo/
+chroot /mnt/gentoo ./post_chroot.sh
